762ea9739e41b63dc891baea8cdbc1f4f114c222
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-04-03 16:04:42 -0400

    assignment 1 Duel: commit 6, add comments and fix mistakes

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -6,7 +6,7 @@
 
 (function(){
 
-    console.log("FIGHT!")
+    console.log("FIGHT!")    //title to signfiy start
 
     var playerOneName="Spiderman";      //players names
     var playerTwoName="Batman";         //players names
@@ -17,31 +17,31 @@
     var playerOneHealth=100;       //players health
     var playerTwoHealth=100;       //players health
 
-    var round =0;                 //variable to show section of time.
+    var round =0;                 //variable to show start of round
 
 function fight(){
 
 
-alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth);
+alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth);   //alert to show the display of the Player and their health
 
 
 
-    for (var i=0; i <10; i++){
+    for (var i=0; i <10; i++){   //to change the number of rounds
 
-        var minDamage1=playerOneDamage *.5;
+        var minDamage1=playerOneDamage *.5;                             //to create a random number, to be deducted or subtracted
         var minDamage2=playerTwoDamage *.5;
         var f1=Math.floor(Math.random()*(playerOneDamage-minDamage1)+minDamage1);
         var f2=Math.floor(Math.random()*(playerTwoDamage-minDamage2)+minDamage2);
 
-        playerOneHealth-=f1;
-        playerTwoHealth-=f2;
+        playerOneHealth-=f1;    //this is how damage gets decucted for player 1
+        playerTwoHealth-=f2;    //this is how damage gets decucted for player 2
 
         console.log(playerOneName+":"+playerOneHealth +" "+playerTwoName+":"+playerTwoHealth);
 
-        var result= winnerCheck();
+        var result= winnerCheck();    //used to call the argument out of the function, and be used elsewhere
         console.log(result);
 
-        if (result ==="no winner"){
+        if (result ==="no winner"){                 //to display the end of the rounds after each click of the the ALERT, also to state that there is still no winner
             round++;
             alert(playerOneName+":"+playerOneHealth+"  -Round "+round+ " Over-  "+ playerTwoName+":"+playerTwoHealth);
         }else{
@@ -57,14 +57,14 @@ alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoH
 function winnerCheck(){
 
     console.log("in winnerCheck Function");
-   var result="no winner";
-
+   var result="no winner";                              //function used to display the Else If statements, If both the
+                                                        //     score are zero or below then, then 1st If statement is TRUE.
     if (playerOneHealth<=0 && playerTwoHealth<=0){
-    result="Both are Dead";
-    }else if(playerOneHealth<=0){
+    result="You Both are Dead";
+    }else if(playerOneHealth<=0){                       //the Else If states if player 1 health is 0, then player 2 wins
     result=playerTwoName+" Wins!"
-    }else if(playerTwoHealth<=0){
-    result=playerOne+" Wins!"
+    }else if(playerTwoHealth<=0){                       //the Else If states if player 2 health is 0, then the player 1 wins
+    result=playerOneName+" Wins!"
     };
 
 
