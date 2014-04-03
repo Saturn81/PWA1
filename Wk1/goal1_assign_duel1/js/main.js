0f302a15d51a6c1648f306ab2434e9850dd58175
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-04-03 11:15:07 -0400

    assignment 1: duel, commit 1

---
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,21 @@
+/*    By: Jeremy Suika
+      Date: 4/3/2014
+        Assignment: Goal1: Assignment Duel 1
+
+    */
+
+(function(){
+
+    console.log("FIGHT!")
+
+    var playerOneName="Spiderman";      //players names
+    var playerTwoName="Batman";         //players names
+
+    var player1Damage= 30;      //players damage
+    var plater2Damage=30;       //players damage
+
+    var player1Health=100;       //players health
+    var plater2Health=100;       //players health
+
+})();
+
