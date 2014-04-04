4fd9227b9089c9c8cd6da6f8bee10bf273915153
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-04-03 21:57:21 -0400

    Goals2 Aggn. Duel 2, set up array and replaced old variables

--- Wk1/goal2_assign_duel2 /js/main.js
+++ Wk1/goal2_assign_duel2 /js/main.js
@@ -7,21 +7,12 @@
 (function(){
 
 
-    var warrior1 =["Spiderman",25,100];
-    var warrior2=["Batman",20,100];
-
-console.log(warrior1[0]);
 
     console.log("FIGHT!");    //title to signify start
 
-    var playerOneName="Spiderman";      //players names
-    var playerTwoName="Batman";         //players names
+   var warrior1 =["Spider-Man",25,100];   //array used to for name of superhero, damage, and total beginning health
+   var warrior2=["Batman",25,100];          //array used to for name of superhero, damage, and total beginning health
 
-   var playerOneDamage= 20;      //players damage
-   var playerTwoDamage=20;       //players damage
-
-   var playerOneHealth=100;       //players health
-   var playerTwoHealth=100;       //players health
 
     var round =0;                 //variable to show start of round
 
@@ -34,22 +25,22 @@ alert(warrior1[0]+":"+warrior1[2]+" *START* "+warrior2[0]+":"+warrior2[2]);   //
 
     for (var i=0; i <10; i++){   //to change the number of rounds
 
-        var minDamage1=playerOneDamage *.5;                             //to create a random number, to be deducted or subtracted
-        var minDamage2=playerTwoDamage *.5;
-        var f1=Math.floor(Math.random()*(playerOneDamage-minDamage1)+minDamage1);
-        var f2=Math.floor(Math.random()*(playerTwoDamage-minDamage2)+minDamage2);
+        var minDamage1=warrior1[1] *.5;                             //to create a random number, to be deducted or subtracted
+        var minDamage2=warrior2[1] *.5;
+        var f1=Math.floor(Math.random()*(warrior1[1]-minDamage1)+minDamage1);
+        var f2=Math.floor(Math.random()*(warrior2[1]-minDamage2)+minDamage2);
 
-        playerOneHealth-=f1;    //this is how damage gets decucted for player 1
-        playerTwoHealth-=f2;    //this is how damage gets decucted for player 2
+        warrior1[2]-=f1;    //this is how damage gets decucted for player 1
+        warrior2[2]-=f2;    //this is how damage gets decucted for player 2
 
-        console.log(playerOneName+":"+playerOneHealth +" "+playerTwoName+":"+playerTwoHealth);
+        console.log(warrior1[0]+":"+warrior1[2] +" "+warrior2[0]+":"+warrior2[2]);
 
         var result= winnerCheck();    //used to call the argument out of the function, and be used elsewhere
         console.log(result);
 
         if (result ==="no winner"){                 //to display the end of the rounds after each click of the the ALERT, also to state that there is still no winner
             round++;
-            alert(playerOneName+":"+playerOneHealth+"  -Round "+round+ " Over-  "+ playerTwoName+":"+playerTwoHealth);
+            alert(warrior1[0]+":"+warrior1[2]+"  -Round "+round+ " Over-  "+ warrior2[0]+":"+warrior2[2]);
         }else{
             alert(result);
             break;
@@ -65,12 +56,12 @@ function winnerCheck(){
 
    var result="no winner";                              //function used to display the Else If statements, If both the
                                                         //     score are zero or below then, then 1st If statement is TRUE.
-    if (playerOneHealth<=0 && playerTwoHealth<=0){
+    if (warrior1[2]<=0 && warrior2[2]<=0){
     result="You Both are Dead";
-    }else if(playerOneHealth<=0){                       //the Else If states if player 1 health is 0, then player 2 wins
-    result=playerTwoName+" Wins!"
-    }else if(playerTwoHealth<=0){                       //the Else If states if player 2 health is 0, then the player 1 wins
-    result=playerOneName+" Wins!"
+    }else if(warrior1[2]<=0){                       //the Else If states if player 1 health is 0, then player 2 wins
+    result=warrior2[0]+" Wins!"
+    }else if(warrior2[2]<=0){                       //the Else If states if player 2 health is 0, then the player 1 wins
+    result=warrior1[0]+" Wins!"
     };
 
 
