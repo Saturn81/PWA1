675f29aa2febf0381410c3acae47611432a4bde2
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-04-03 22:04:55 -0400

    Goals 2, Duel 2, tested, and made changes to wording

--- Wk1/goal2_assign_duel2 /js/main.js
+++ Wk1/goal2_assign_duel2 /js/main.js
@@ -10,8 +10,8 @@
 
     console.log("FIGHT!");    //title to signify start
 
-   var warrior1 =["Spider-Man",25,100];   //array used to for name of superhero, damage, and total beginning health
-   var warrior2=["Batman",25,100];          //array used to for name of superhero, damage, and total beginning health
+   var warrior1 =["Spider-Man",20,100];   //array used to for name of superhero, damage, and total beginning health
+   var warrior2=["Batman",20,100];          //array used to for name of superhero, damage, and total beginning health
 
 
     var round =0;                 //variable to show start of round
@@ -23,15 +23,15 @@ alert(warrior1[0]+":"+warrior1[2]+" *START* "+warrior2[0]+":"+warrior2[2]);   //
 
 
 
-    for (var i=0; i <10; i++){   //to change the number of rounds
+    for (var i=0; i <10; i++){   //to change the number of rounds, not to go over 10, for the loop
 
         var minDamage1=warrior1[1] *.5;                             //to create a random number, to be deducted or subtracted
         var minDamage2=warrior2[1] *.5;
         var f1=Math.floor(Math.random()*(warrior1[1]-minDamage1)+minDamage1);
         var f2=Math.floor(Math.random()*(warrior2[1]-minDamage2)+minDamage2);
 
-        warrior1[2]-=f1;    //this is how damage gets decucted for player 1
-        warrior2[2]-=f2;    //this is how damage gets decucted for player 2
+        warrior1[2]-=f1;    //this is how damage gets deducted for player 1
+        warrior2[2]-=f2;    //this is how damage gets deducted for player 2
 
         console.log(warrior1[0]+":"+warrior1[2] +" "+warrior2[0]+":"+warrior2[2]);
 
