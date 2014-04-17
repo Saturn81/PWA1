925be728f2fc36293436029e0db09782602f0f52
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-04-16 23:18:07 -0400

    goal4_assign_duel3--input functions

--- Wk3/goals4_assign_duel3/js/main.js
+++ Wk3/goals4_assign_duel3/js/main.js
@@ -12,10 +12,32 @@
     var warrior1_txt =document.querySelector("#kratos").querySelector("p")
     var warrior2_txt =document.querySelector("#kabal").querySelector("p")
     var round_txt=document.querySelector("h4");
-    var button= document.getElementById("Fight_btn");
+    var button= document.getElementById("fight_btn");
 
 
-    button.add
+    button.addEventListener("Click",Fight, false);
+
+    var warriors = [
+        {
+            name:"Spiderman",damage:25,health:100
+
+        },
+        {
+            name:"Bane",damage:20,health:100
+
+
+        }];
+    var round=1;
+
+
+    round_txt.innerHTML="Click Here to Start Fight";
+    warrior1_txt.innerHTML=warriors[0].name + ":" + warriors[0].health;
+    warrior1_txt.innerHTML=warriors[1].name + ":" + warriors[1].health;
+
+
+
+
+
 
 
 
