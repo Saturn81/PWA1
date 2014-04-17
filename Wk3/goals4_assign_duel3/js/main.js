be0ca5e7eaafa8a5de48ed23b4e58356b745772c
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-04-17 00:45:45 -0400

    goal4_assign_duel3-add more functions, get error

--- Wk3/goals4_assign_duel3/js/main.js
+++ Wk3/goals4_assign_duel3/js/main.js
@@ -34,28 +34,55 @@
     warrior1_txt.innerHTML=warriors[0].name + ":" + warriors[0].health;
     warrior1_txt.innerHTML=warriors[1].name + ":" + warriors[1].health;
 
+   function fightOn(){
+
+       warrior1_txt.innerHTML=warriors[0].name + ":" + warriors[0].health;
+       warrior2_txt.innerHTML=warriors[1].name + ":" + warriors[1].health;
+
+       var f1=Math.floor(Math.random() * warriors[0].damage+warriors[0].damage *.50);
+       var f2=Math.floor(Math.random() * warriors[1].damage+warriors[1].damage *.50);
 
 
+       warriors[0].health -= f1;
+       warriors[1].health -= f2;
 
-    var f1=Math.floor(Math.random() * warriors[0].damage+warriors[0].damage *.50);
-    var f2=Math.floor(Math.random() * warriors[1].damage+warriors[1].damage *.50);
-
-
-    warriors[0].health -= f1;
-    warriors[1].health -= f2;
-
-
-
-
-
-
-
-
+       console.log(warriors[0].health, warriors[1].health);
 
 
 
 
+       var end =whoWon();
+       console.log(end);
 
+
+       round_txt.innerHTML= "Round" + round + "Complete";
+       round++;
+
+       if (end === "no winner");
+       {
+           warrior1_txt.innerHTML=warriors[0].name + ":" + warriors[0].health;
+           warrior2_txt.innerHTML=warriors[1].name + ":" + warriors[1].health;
+
+       }else{
+           warrior1_txt.innerHTML=end;
+           warrior2_txt.innerHTML="";
+
+           document.querySelector('.buttonblue').innerHTML="Done!";
+
+       }
+
+
+
+
+   }
+
+
+
+
+
+
+
+    console.log(warriors[0].health);
 
 
 
